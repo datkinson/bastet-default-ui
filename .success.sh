2886faa1eb8ede73efa52221d930b72ae311b19c
Daniel Atkinson <hourd.tasa@gmail.com>
2015-09-14 21:32:22 +0100

    scp

--- .success.sh
+++ .success.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+mkdir -p ~/.ssh
+echo "$key" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null index.html $user@$host:.
+rm -rf ~/.ssh
