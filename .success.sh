a9859fdb0b7c02bb146e75197325940092681350
Daniel Atkinson <hourd.tasa@gmail.com>
2015-09-14 21:22:09 +0100

    different approach to ssh

--- .success.sh
+++ .success.sh
@@ -1,15 +1,1 @@
-#!/usr/bin/expect -f
-
-# connect via scp
-spawn scp index.html "$user@$host:."
-#######################
-expect {
--re ".*es.*o.*" {
-exp_send "yes\r"
-exp_continue
-}
--re ".*sword.*" {
-exp_send "$password\r"
-}
-}
-interact
+#!/bin/bash
