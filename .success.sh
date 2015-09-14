5d1dd72b30822ff0ff85e9179b1356331cb8e275
Daniel Atkinson <hourd.tasa@gmail.com>
2015-09-14 21:12:20 +0100

    testing scp

--- .success.sh
+++ .success.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 echo "user: $user"
+# connect via scp
+spawn scp index.html "$user@$host:."
+#######################
+expect {
+-re ".*es.*o.*" {
+exp_send "yes\r"
+exp_continue
+}
+-re ".*sword.*" {
+exp_send "$password\r"
+}
+}
+interact
